e3c6a761edf80b348a08dbb4d55d1e198105a908
Pankaj Singh2 <pankajsingh2@virtual.local>
2017-06-09 14:03:36 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script>
+	$( window ).load(function() {
+		console.log( "window loaded" );
+    });
+    $( document ).ready(function() {
+		console.log( "document loaded" );
+    });
+    </script>
+</head>
+<body>
+   
+</body>
+</html>
